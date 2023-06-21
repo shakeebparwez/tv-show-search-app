fa0bb8490e29a9137f87fc11b12ceb6ec8b672b4
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-06-21 19:51:43 +0530

    Fix the enter key issue

--- app.js
+++ app.js
@@ -4,32 +4,40 @@ const tbody = document.getElementById('tBody');
 
 form.addEventListener('keyup', async (e) => {
 	e.preventDefault();
-	tbody.innerText = '';
-    const searchTerm = form.elements.query.value;
-    const config = { params: { q: searchTerm } }
-    const res = await axios.get(`https://api.tvmaze.com/search/shows/`, config)
+	if (e.key !== "Enter") {
+		tbody.innerText = '';
+		const searchTerm = form.elements.query.value;
+		const config = { params: { q: searchTerm } }
+		const res = await axios.get(`https://api.tvmaze.com/search/shows/`, config)
 
-	for (let i = 0; i < res.data.length; i++) {
-		const tr = document.createElement('tr');
-		const titleName = document.createElement('td');
-		const summary = document.createElement('td');
-		const image = document.createElement('img');
+		for (let i = 0; i < res.data.length; i++) {
+			const tr = document.createElement('tr');
+			const titleName = document.createElement('td');
+			const summary = document.createElement('td');
+			const image = document.createElement('img');
 
-		// image.classList.add('image-box');
-		try {
-			image.src = await res.data[i].show.image.medium;
-		} catch {
-			image.src = 'image.png';
+			// image.classList.add('image-box');
+			try {
+				image.src = await res.data[i].show.image.medium;
+			} catch {
+				image.src = 'image.png';
+			}
+
+			tr.append(image);
+			tr.append(titleName);
+			titleName.innerText = await res.data[i].show.name;
+			tr.append(summary);
+			summary.innerHTML = await res.data[i].show.summary;
+			summary.classList.add('summary');
+
+			tbody.append(tr);
+			// userInput.value = '';
 		}
-
-		tr.append(image);
-		tr.append(titleName);
-		titleName.innerText = await res.data[i].show.name;
-		tr.append(summary);
-		summary.innerHTML = await res.data[i].show.summary;
-		summary.classList.add('summary');
-
-		tbody.append(tr);
-		// userInput.value = '';
 	}
+});
+
+form.addEventListener('submit', async function (e) {
+	e.preventDefault();
+	// console.dir(userInput);
+	userInput.value = "";
 });
